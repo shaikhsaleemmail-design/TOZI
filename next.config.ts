2562ed743582a772995bfade7c9d7dc00ef67500
shaikhsaleemmail-design <shaikhsaleemmail@gmail.com>
2026-05-28 16:00:06 +0530

    ignore typescript build errors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
